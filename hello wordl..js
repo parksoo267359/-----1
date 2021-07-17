09887c27eb074eed1ed539b7e9ba5ad17de4bad5
parksoo267359 <parksoo267359@gmail.com>
2021-07-17 13:53:25 +0900

    first commit

---
+++ hello wordl..js
@@ -0,0 +1,46 @@
+alert('zzz')
+var name = 'kim'
+let name2="un"
+alert(name);
+alert(name2);
+let name="kim"
+const name = "kim"
+//var let const
+//var,let=변수를 만들고 자유롭게 수정이 가능하다
+//const=변수를 만들고 수정이 불가능하다.
+//var은 구리다 let랑 const만 있다. 
+let number=1; //정수
+let float=3.14; //소수
+let string='kim' //문자열
+let bool = false; //참거짓
+let bool2 = true;
+
+console.log(number+string);
+console.log(1/0);
+console.log(tmp);
+console.log(float+string);
+console.log(1*string)
+console.log(1/0);
+console.log(tmp);
+let number = 133333333333333333333333333333333n;
+let string='철수의 키'
+let float=180.4;
+console.log(string+float);
+console.log('철수의 키는:'float+입니다");  
+console.log('철수의 키는' ${} 입니다.); //es6 최신
+
+console.log(string(number));
+console.log(Number(string)) //자료형 변환 , 간단한 연산자 정리 
+console.log(number+float);
+console.log(number-float);
+console.log(number/float);
+console.log(number%float);
+console.log(number**float);
+console.log(number);
+console.log(number--); //자신한테 +1 -1 ++ -- 11 10이다.
+let a = (1+2+3+4);
+console.log(a);
+
+
+
+
