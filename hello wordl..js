2980d62939d12426373174fba64acad02d326a4c
park soo hyeon <parksoo267359@naver.com>
2021-07-27 13:24:29 +0900

    변경사항

--- hello wordl..js
+++ hello wordl..js
@@ -41,6 +41,104 @@ console.log(number--); //자신한테 +1 -1 ++ -- 11 10이다.
 let a = (1+2+3+4);
 console.log(a);
 
+let kim = 'kim'
+let lee = 'lee'
+lee = 'kim';
+if(lee===kim){
+    console.log('같아'); rkxdk
+}else if(lee=='lee'){
+    console.log('중복이야');
+}else{
+    console.log('그런 이름한 적 없어')
+}else{
+    console.log('그런 이름한 적 없어')
+}
+let age = 19;
+let message = (age<19)? "청소년 이용 불가입니다." : "성인 안녕";
+console.log(message)
+console.log(message) 청소년 이용 불가입니다. 
+let gender = '남자'
+//19살 성인 남자라면 출입 불가능 
+//19살 성인 여자면 출입 가능으로 코딩으로 만든다. 
+let massage = (age>19 && gender ==="남자")? "남자 이용불가이빈다.":"안녕";
+console.log(message) //안녕 
+//&& and
+// :: or
+if() i is not defined
+else if()
+else 
+//남자인지 여자인지 보고 if 성별 체크 
+if(나이가 50이하 이고 10살 이상 인 사람)
+else if(성별이 남자이고 10살 미만인 사람) 
+    console.log("애기");
+else
+    console.log("아무것나");
+}else{
+    console.log
+}
+
+let gender ='남자';
+let age=11;
+
+if(age>=10 && gender ==='남자'){
+    console.log('꼬맹이');
+}else if(age < 10 && gender === "남자")
+    console.log('애기'); 
+else
+    console.log('여성입니다.')
+
+let name = '';
+
+var Name = '';
+if(name==null){
+    name=='kim'
+console.log(Name);
+}
+
+let height;
+height=height??100;
+console.log(height); 100
+
+if(height===undefined){
+// height=100;
+//}
+
+height=height??100;
+//console.log(height);
+
+let count=0;
+console.log(count);
+count++;
+count++;
+count++;
+count++;
+count++;
+count++;
+count++;
+count++;count++;
+
+//반복문에는 for문이 있고 두번째로는 while문이 있다. 
+while(count<10){
+    count++;
+}
+console.log(count);
+let trigger=false;
+while(true){    
+}
+console.log(count);
+if(count==10) break;
+}
+console.log('count가 10이 되었습니다. 반복문 종료');
+    break;
+    }
+}  
+for(var i=1; i<10; i++){
+    console.log(i)
+    }
+}
+
+<script>
 
 
 
+</script>
